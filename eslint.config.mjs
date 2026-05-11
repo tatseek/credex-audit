3feddcbabd44d2bcc3f017476132dd3a56f56fb8
Arunima Das <das31arunima@gmail.com>
2026-05-11 07:48:24 +0530

    updated CI to use node 22 and skipped lint step

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,8 +13,9 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-explicit-any": "off",
       "@next/next/no-html-link-for-pages": "off",
+      "react/no-unescaped-entities": "off",
     },
   },
 ];
